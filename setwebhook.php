39cef388b44eb2494fe28f2d6828e204ad490b98
Artyom <artyom.koranskorabahaty@gmail.com>
2022-02-11 19:37:40 +0300

    Добавил проверку php

--- setwebhook.php
+++ setwebhook.php
@@ -1,11 +1,11 @@
 <?php
-// 123 git подключение
+// подключён git, heroku
 const TOKEN ="2105487870:AAGOOToNtTEY-5rTZLhztPC0_zlPtU70u0k";
 $method = 'setWebhook';
 
 $url = 'https://api.telegram.org/bot' . TOKEN . '/'  . $method;
 $options = [
-  'url' => '',
+  'url' => 'https://git.heroku.com/telegrambotgopnick.git',
 ];
 
 $response = file_get_contents($url . '?' . http_build_query($options));
