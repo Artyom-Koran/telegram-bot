23fda60c156052f57f05c8fc5780aa25ae3e48bb
Artyom <artyom.koranskorabahaty@gmail.com>
2022-03-05 17:17:37 +0300

    освоил composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c91c7b8e610f21168fc92b864f8e9f04969f2732',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c91c7b8e610f21168fc92b864f8e9f04969f2732',
+            'dev_requirement' => false,
+        ),
+    ),
+);
