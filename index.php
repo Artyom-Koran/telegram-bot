0fb47ed9d3f566a109266ba5620f808fee8034ad
Artyom <artyom.koranskorabahaty@gmail.com>
2022-03-06 15:41:11 +0300

    no comments

--- index.php
+++ index.php
@@ -1,31 +1,5 @@
+
 <?php
-const TOKEN = '2105487870:AAGOOToNtTEY-5rTZLhztPC0_zlPtU70u0k';
-const BASE_URL = 'https://api.telegram.org/bot'. TOKEN . '/';
-
-$update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
-
-function sendRequest($method, $params = []){
-    if(!empty($params)){
-      $url = BASE_URL . $method . '?'. http_build_query($params);
-    } else {
-      $url = BASE_URL . $method;
-    }
-    return json_decode(
-        file_get_contents($url),
-        JSON_OBJECT_AS_ARRAY
-    );
-
-}
-
-
-//$updates = sendRequest('getUpdates');
-
-$time = date('H:m:s');
-
-$chat_id = $update['message']['chat']['id'];
-sendRequest('sendMessage', ["chat_id" => $chat_id, "text"=> $time . " - время"]);
-
-
- //var_dump( sendRequest('sendMessage', ["chat_id" => $chat_id, "text"=>"Ответ"]) );
+file_put_contents(__DIR__ . /log.txt, file_get_contents('php://input'));
 
  ?>
