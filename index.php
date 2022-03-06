f7d58921f3a61491db06c01fcbcd745b347b1d5b
Artyom <artyom.koranskorabahaty@gmail.com>
2022-03-06 14:03:23 +0300

    изменение index.php

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 <?php
-// для сервера
-echo "работает";
-
 file_put_contents(__DIR__ . '/log.txt', file_get_contents('php://input'));
 ?>
--- test.php
+++ test.php
@@ -2,6 +2,8 @@
 const TOKEN = '2105487870:AAGOOToNtTEY-5rTZLhztPC0_zlPtU70u0k';
 const BASE_URL = 'https://api.telegram.org/bot'. TOKEN . '/';
 
+$update = json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
+
 function sendRequest($method, $params = []){
     if(!empty($params)){
       $url = BASE_URL . $method . '?'. http_build_query($params);
@@ -15,14 +17,14 @@ function sendRequest($method, $params = []){
 
 }
 
-$updates = sendRequest('getUpdates');
+
+//$updates = sendRequest('getUpdates');
 
 $time = date('H:m:s');
 
-foreach ($updates['result'] as $update) {
-  $chat_id = $update['message']['chat']['id'];
-  sendRequest('sendMessage', ["chat_id" => $chat_id, "text"=> $time . " - время"]);
-}
+$chat_id = $update['message']['chat']['id'];
+sendRequest('sendMessage', ["chat_id" => $chat_id, "text"=> $time . " - время"]);
+
 
  //var_dump( sendRequest('sendMessage', ["chat_id" => $chat_id, "text"=>"Ответ"]) );
 
